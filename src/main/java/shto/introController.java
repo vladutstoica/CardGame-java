de672971f875ab8f360354f95e8e7ce8d786ae65
iamSHTO <jokeerioro@gmail.com>
2019-12-05 15:55:43 +0200

    update intro window

--- src/main/java/shto/introController.java
+++ src/main/java/shto/introController.java
@@ -1,14 +1,22 @@
 package shto;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class introController {
 
+    public TextField credit;
+    public TextField infoBox;
+
     @FXML // so we can have private methods
     private void goToTheGame() throws IOException {
-        App.setRoot("main");
+        if (credit.getText().trim().isEmpty()) {
+            infoBox.setText("ENTER A NUMBER");
+        } else if (credit.getText().matches("[0-9]+")) {
+            App.setRoot("main");
+        }
     }
 
 }
