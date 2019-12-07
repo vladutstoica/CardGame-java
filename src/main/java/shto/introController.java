33bf29ebb69c3cc424150b195f21571ad11de8c4
Vlad Mihai <jokeerioro@gmail.com>
2019-12-08 00:11:02 +0200

    error free

--- src/main/java/shto/introController.java
+++ src/main/java/shto/introController.java
@@ -1,23 +1,32 @@
 package shto;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class introController {
 
-    public TextField credit;
-    public TextField infoBox;
+    public Button button;
 
-    @FXML // so we can have private methods
-    private void goToTheGame() throws IOException {
-        if (credit.getText().trim().isEmpty() || !credit.getText().matches("[0-9]+")) {
-            infoBox.setText("ENTER A NUMBER");
-        } else if (credit.getText().matches("[0-9]+")) {
-            mainController.setName(credit.getText());
-            App.setRoot("main");
-        }
+    public void goToTheGame() throws IOException {
+        //if (credit.getText().trim().isEmpty() || !credit.getText().matches("[0-9]+")) {
+        //    infoBox.setText("ENTER A NUMBER");
+        //} else {
+        //    mainController.setName("test");
+        //    App.setRoot("main");
+        //}
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        loader.load();
+
+        // Get controller of scene2
+        mainController mainController = loader.getController();
+
+        // Pass whatever data you want. You can have multiple method calls here
+
+        App.setRoot("main");
+        button.setOnAction(e -> mainController.setName("test"));
+        //mainController.setName("test");
+
     }
-
 }
--- src/main/java/shto/mainController.java
+++ src/main/java/shto/mainController.java
@@ -1,22 +1,12 @@
 package shto;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class mainController {
 
-    @FXML
-    private static TextField mainCredit;
+    public TextField mainCredit;
 
-    public static void setName(String name) {
-        //mainCredit.setText(name);
-        if (name != null) {
-            //do something
-            System.out.print("not null");
-            mainCredit.setText(name);
-        } else {
-            //do something else
-            System.out.print("null");
-        }
+    public void setName(String name) {
+        mainCredit.setText(name);
     }
 }
