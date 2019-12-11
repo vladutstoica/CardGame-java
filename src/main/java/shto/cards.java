03f5582171a90ae318a31ef9edbd9be54773c99e
iamSHTO <jokeerioro@gmail.com>
2019-12-11 19:09:02 +0200

    create special class for images

---
+++ src/main/java/shto/cards.java
@@ -0,0 +1,61 @@
+package shto;
+
+public class cards {
+    String[] cardRedImg = {
+            "cards/ace_of_hearts.png",
+            "cards/ace_of_diamonds.png",
+            "cards/2_of_hearts.png",
+            "cards/2_of_diamonds.png",
+            "cards/3_of_hearts.png",
+            "cards/3_of_diamonds.png",
+            "cards/4_of_hearts.png",
+            "cards/4_of_diamonds.png",
+            "cards/5_of_hearts.png",
+            "cards/5_of_diamonds.png",
+            "cards/6_of_hearts.png",
+            "cards/6_of_diamonds.png",
+            "cards/7_of_hearts.png",
+            "cards/7_of_diamonds.png",
+            "cards/8_of_hearts.png",
+            "cards/8_of_diamonds.png",
+            "cards/9_of_hearts.png",
+            "cards/9_of_diamonds.png",
+            "cards/10_of_hearts.png",
+            "cards/10_of_diamonds.png",
+            "cards/jack_of_hearts.png",
+            "cards/jack_of_diamonds.png",
+            "cards/queen_of_hearts.png",
+            "cards/queen_of_diamonds.png",
+            "cards/king_of_hearts.png",
+            "cards/king_of_diamonds.png"
+    };
+
+    String[] cardBlackImg = {
+            "cards/ace_of_spades.png",
+            "cards/ace_of_clubs.png",
+            "cards/2_of_spades.png",
+            "cards/2_of_clubs.png",
+            "cards/3_of_spades.png",
+            "cards/3_of_clubs.png",
+            "cards/4_of_spades.png",
+            "cards/4_of_clubs.png",
+            "cards/5_of_spades.png",
+            "cards/5_of_clubs.png",
+            "cards/6_of_spades.png",
+            "cards/6_of_clubs.png",
+            "cards/7_of_spades.png",
+            "cards/7_of_clubs.png",
+            "cards/8_of_spades.png",
+            "cards/8_of_clubs.png",
+            "cards/9_of_spades.png",
+            "cards/9_of_clubs.png",
+            "cards/10_of_spades.png",
+            "cards/10_of_clubs.png",
+            "cards/jack_of_spades.png",
+            "cards/jack_of_clubs.png",
+            "cards/queen_of_spades.png",
+            "cards/queen_of_clubs.png",
+            "cards/king_of_spades.png",
+            "cards/king_of_clubs.png"
+    };
+}
